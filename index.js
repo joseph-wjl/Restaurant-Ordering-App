ab64608eee195af51d7b7fa1c56faa1af560bc5f
Joseph Lau <lau.joseph.ca@gmail.com>
2025-01-04 20:27:36 +0800

    edit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import { menuArray } from "./data.js"
 
+const header = document.getElementById("header")
 const foodList = document.getElementById("food-list")
+const addBtn = document.getElementById("add-btn")
 
-
-function renderMenuItems(){
+function renderMenuItems() {
     let menuItems = ``
-    for (let item of menuArray){
+    for (let item of menuArray) {
         menuItems += `
             <div class="food-items">
                 <img src="${item.img}">
                 <h2>${item.name}</h2>
                 <p>${item.ingredients}</p>
                 <h2>$${item.price}</h2>
-                <button class="add-button">+</button>
+                <button class="add-btn" id="add-btn">+</button>
             </div>
         `
     }
@@ -21,26 +22,6 @@ function renderMenuItems(){
 
 renderMenuItems()
 
-// function renderMenuItems() {
-//     menuArray.forEach(function(menuItem) {
-//         // Append content to the innerHTML, don't overwrite it
-//         foodList.innerHTML += `
-//             <div class="food-item">
-//                 <img src="${menuItem.img}" alt="${menuItem.name}" class="food-images">
-//                 <h2>${menuItem.name}</h2>
-//                 <p>${menuItem.ingredients.join(", ")}</p>
-//                 <h3>$${menuItem.price}</h3>
-//             </div>
-//         `
-//     })
-// }
-
-// renderMenuItems()
-
-
-// menuArray.forEach(function (menuItem) {
-//     const foodListContent = ``
-//     foodList.innerHTML = foodListContent + `
-    
-//     `
-// })
+addBtn.addEventListener("click", function () {
+    header.innerHTML = `button clicked`
+})
